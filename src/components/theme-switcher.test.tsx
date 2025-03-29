2877e4f02d43f14a82c946c370d4d09e03ca760e
Pongstr <twiz.tickler@gmail.com>
2025-03-29 18:56:05 +0200

    chore: React 19 upgrade

--- src/components/theme-switcher.test.tsx
+++ src/components/theme-switcher.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
-import { act } from 'react'
 import { describe, expect, it } from 'vitest'
 
 import ThemeSwitcher from '@/components/theme-switcher'
@@ -19,15 +18,15 @@ describe('<ThemeSwitcher /> Component', () => {
       'dark',
     )
 
-    await userEvent.click(screen.getByTestId('trigger'))
-
+    userEvent.click(screen.getByTestId('trigger'))
     expect(screen.getByTestId('theme-button-dark')).toBeInTheDocument()
 
-    await userEvent.click(screen.getByTestId('theme-button-dark')),
-      expect(screen.getByTestId('trigger')).toHaveAttribute(
-        'data-selected-theme',
-        'dark',
-      )
+    userEvent.click(screen.getByTestId('theme-button-dark'))
+
+    expect(screen.getByTestId('trigger')).toHaveAttribute(
+      'data-selected-theme',
+      'dark',
+    )
     expect(screen.getByTestId('theme-switch-container')).toHaveAttribute(
       'data-theme',
       'dark',
